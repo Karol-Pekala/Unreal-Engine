1d3efc89ec1916642d3a5dd164767dad13a73310
Karol-Pekala <97751383+Karol-Pekala@users.noreply.github.com>
2022-07-16 12:54:59 +0200

    Delete JumpShooting directory

--- JumpShooting/Source/JumpShooting/JumpShooting.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "JumpShooting.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JumpShooting, "JumpShooting" );
- 
